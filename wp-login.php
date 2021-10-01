feff7af237872dc0608d1c399e01d56a7188ec40
HoiNhungNguoiDamMeBongDaVN <KhaHoTraVinh2002@gmail.com>
2021-10-01 15:50:31 +0700

    adc

--- wp-login.php
+++ wp-login.php
@@ -90,7 +90,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress-5.8\/wordpress\/wp-
 <script type="text/javascript" src="https://hoinhungnguoidammebongdavn.github.io/MyWordpres/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"47b9f5ecb9"};
+var userProfileL10n = {"user_id":"0","nonce":"cf3aefda3b"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
